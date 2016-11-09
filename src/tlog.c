4e44b3d2a4c330b4df363d38550926e7fb624003
Noktoborus <noktoborus@gmail.com>
2016-11-09 12:34:57 +0300

    add time to tlog

--- src/tlog.c
+++ src/tlog.c
@@ -3,6 +3,7 @@
  */
 #include <stdio.h>
 #include <stdarg.h>
+#include <time.h>
 
 #include "tlog.h"
 
@@ -17,11 +18,18 @@ tlog_open()
 void
 _tlog(const char *parent_file, const char *parent_func, const char *format, ...)
 {
+	time_t t = 0u;
+	char tm_str[24] = {};
+	struct tm *tm = NULL;
 	va_list ap = {};
 	if (!log)
 		return;
+
+	time(&t);
+	tm = gmtime(&t);
+	strftime(tm_str, sizeof(tm_str), "%b %e %T", tm);
 	/* begin */
-	fprintf(log, "[] [parent %s:%s] ", parent_file, parent_func);
+	fprintf(log, "[%s] [parent %s:%s] ", tm_str, parent_file, parent_func);
 	/* data */
 	va_start(ap, format);
 	vfprintf(log, format, ap);
