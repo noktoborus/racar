979a0536a05a49a0387e8d3ca69b29f3775b0fde
Noktoborus <noktoborus@gmail.com>
2016-11-17 14:52:03 +0300

    add mempool

---
+++ src/mempool.c
@@ -0,0 +1,243 @@
+/* vim: ft=c ff=unix fenc=utf-8
+ * file: src/mempool.c
+ */
+#include <errno.h>
+#include <string.h>
+
+#include "mempool.h"
+
+static void
+_clean_node(struct mmp_node *mn)
+{
+	if (!mn) {
+		return;
+	}
+
+	switch (mn->type) {
+		case MMP_NORMAL:
+			break;
+		case MMP_GENERIC:
+			if (mn->v.gen.free) {
+				mn->v.gen.free(mn->v.gen.data);
+			}
+			break;
+		case MMP_FD:
+			if (mn->v.fd.close) {
+				mn->v.fd.close(mn->v.fd.fd);
+			}
+			break;
+	}
+}
+
+struct mmp *
+mmp_create()
+{
+	struct mmp *m = NULL;
+	m = calloc(1, sizeof(*m));
+	return m;
+}
+
+void mmp_clean(struct mmp *m)
+{
+	struct mmp_node *mn = NULL;
+	struct mmp_node *mn_next = NULL;
+
+	if (!m)
+		return;
+
+	mn = m->next;
+	m->next = NULL;
+	for (; mn; mn = mn_next) {
+		mn_next = mn->next;
+		_clean_node(mn);
+		free(mn);
+	}
+}
+
+void
+mmp_destroy(struct mmp *m)
+{
+	mmp_clean(m);
+	free(m);
+}
+
+static void
+_assign_node(struct mmp *m, struct mmp_node *mn)
+{
+	memset(mn, 0, sizeof(*mn));
+
+	mn->protector = mn;
+
+	if ((mn->next = m->next) != NULL) {
+		mn->next->prev = mn;
+	}
+	m->next = mn;
+
+	mn->m = m;
+}
+
+void *
+mmp_malloc(struct mmp *m, size_t size)
+{
+	struct mmp_node *mn = NULL;
+
+	if (!(mn = malloc(sizeof(*mn) + size))) {
+		return NULL;
+	}
+
+	/* init */
+	mn->type = MMP_NORMAL;
+	mn->v.gen.data = (void*)(mn + 1);
+	mn->size = size;
+
+	_assign_node(m, mn);
+
+	return mn->v.gen.data;
+}
+
+void *mmp_calloc(struct mmp *m, size_t size)
+{
+	struct mmp_node *mn = NULL;
+
+	if (!(mn = calloc(1, sizeof(*mn) + size))) {
+		return NULL;
+	}
+
+	mn->type = MMP_NORMAL;
+	mn->v.gen.data = (void*)(mn + 1);
+	mn->size = size;
+
+	_assign_node(m, mn);
+
+	return mn->v.gen.data;
+}
+
+void *
+mmp_assign(struct mmp *m, void *data, void(*pfree)(void*))
+{
+	struct mmp_node *mn = NULL;
+
+	if (!data)
+		return NULL;
+
+	if (!(mn = calloc(1, sizeof(*mn)))) {
+		pfree(data);
+		return NULL;
+	}
+
+	mn->type = MMP_GENERIC;
+	mn->v.gen.data = data;
+	mn->v.gen.free = pfree;
+	mn->size = sizeof(void*);
+
+	_assign_node(m, mn);
+
+	return mn->v.gen.data;
+}
+
+int mmp_assign_fd(struct mmp *m, int fd, void(*pclose)(int fd))
+{
+	struct mmp_node *mn = NULL;
+
+	if (fd == -1)
+		return false;
+
+	if (!(mn = calloc(1, sizeof(*mn) + sizeof(int)))) {
+		pclose(fd);
+		return -1;
+	}
+
+	mn->type = MMP_FD;
+	mn->v.fd.fd = fd;
+	mn->v.fd.close = pclose;
+	mn->size = sizeof(int);
+
+	_assign_node(m, mn);
+
+	return fd;
+}
+
+static struct mmp_node *
+_get_header(void *data)
+{
+	struct mmp_node *mn = NULL;
+
+	mn = ((struct mmp_node*)data) - 1;
+	if (mn->protector != mn) {
+		return NULL;
+	}
+
+	return mn;
+}
+
+void *
+mmp_realloc(void *data, size_t size)
+{
+	register struct mmp_node *mn_prev = NULL;
+	register struct mmp_node *mn_next = NULL;
+	struct mmp *m = NULL;
+
+	struct mmp_node *mn = NULL;
+	register void *tmp = NULL;
+
+	if (!(mn = _get_header(data))) {
+		return NULL;
+	}
+
+	if (mn != MMP_NORMAL) {
+		return NULL;
+	}
+
+	/* temporary unlink */
+	if ((mn_prev = mn->prev) != NULL) {
+		mn->prev->next = mn->next;
+	}
+	if ((mn_next = mn->next) != NULL) {
+		mn_next->prev = mn->prev;
+	}
+	if ((m = mn->m)->next == mn) {
+		m->next = mn->next;
+	}
+
+	/* realloc */
+	tmp = realloc(mn, sizeof(mn) + size);
+	if (!tmp) {
+		return NULL;
+	}
+
+	mn->v.gen.data = (void*)(mn + 1);
+	mn->size = size;
+
+	_assign_node(m, mn);
+
+	return mn->v.gen.data;
+}
+
+void
+mmp_free(void *data)
+{
+	struct mmp *m = NULL;
+	struct mmp_node *mn = NULL;
+
+	if (!(mn = _get_header(data))) {
+		return;
+	}
+	_clean_node(mn);
+
+	/* unlink */
+	m = mn->m;
+
+	if (m->next == mn) {
+		m->next = mn->next;
+	}
+
+	if (mn->next) {
+		mn->next->prev = mn->prev;
+	}
+	if (mn->prev) {
+		mn->prev->next = mn->next;
+	}
+
+	free(mn);
+}
+
---
+++ src/mempool.h
@@ -0,0 +1,82 @@
+/* vim: ft=c ff=unix fenc=utf-8
+ * file: src/mempool.h
+ */
+#ifndef _SRC_MEMPOOL_1479374773_H_
+#define _SRC_MEMPOOL_1479374773_H_
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+enum mmp_node_type {
+	/* use in mmp_malloc, mmp_calloc */
+	MMP_NORMAL = 0,
+	/* use in mmp_assign */
+	MMP_GENERIC = 1,
+	/* use in mmp_assign_fd */
+	MMP_FD = 2,
+};
+
+struct mmp_node {
+	/* pointer to self */
+	struct mmp_node *protector;
+
+	struct mmp *m;
+
+	char type;
+
+	size_t size;
+
+	struct mmp_node *prev;
+	struct mmp_node *next;
+
+	union {
+		struct {
+			void *data;
+			void (*free)(void*);
+		} gen;
+		struct {
+			int fd;
+			void (*close)(int);
+		} fd;
+	} v;
+};
+
+struct mmp {
+	size_t elements;
+	struct mmp_node *next;
+};
+
+/* create new mmp structure */
+struct mmp *mmp_create();
+/* clean mmp structure */
+void mmp_clean(struct mmp *m);
+/* clean and destroy mmp structure */
+void mmp_destroy(struct mmp *m);
+
+/* allocate memory block */
+void *mmp_malloc(struct mmp *m, size_t size);
+/* allocate and fill zeros */
+void *mmp_calloc(struct mmp *m, size_t size);
+/* assign generic data
+ * return data or NULL when fail
+ * pfree(data) when fail
+ */
+void *mmp_assign(struct mmp *m, void *data, void(*pfree)(void*));
+/* assign file descriptor,
+ * return fd or -1 when fail
+ * pclose(fd) when fail
+ */
+int mmp_assign_fd(struct mmp *m, int fd, void(*pclose)(int fd));
+/* realloc memory block
+ * return new pointer to data or NULL if fail
+ * return NULL when data not have a mmp header
+ * ->free(data) when fail
+ * if node type != MMP_NORMAL, return NULL and not not deallocated
+ */
+void *mmp_realloc(void *data, size_t size);
+/* free data with mmp header */
+void mmp_free(void *data);
+
+#endif /* _SRC_MEMPOOL_1479374773_H_ */
+
