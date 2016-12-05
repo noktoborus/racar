f99d43ff8578c1a8e4ebbe4572f2065ebd08fc11
Noktoborus <noktoborus@gmail.com>
2016-12-05 17:38:00 +0300

    normilize model tree

--- src/main.c
+++ src/main.c
@@ -22,29 +22,13 @@ begin(TL_V)
 	struct rcr rcr = {0};
 	struct mdl mdl = {0};
 
-	struct mdl_node *mn_dst = NULL;
-	struct mdl_node *mn_src = NULL;
-
 	/* init */
 	mdl_init(TL_A, &mdl);
 	mm_initialize(TL_A);
 	mdl_set_allocator(TL_A, &mdl, mm_model_allocator, mm_model_deallocator, mm_model_copier, mdl.mmp);
 
 	/* work */
-	mdl_add_path(TL_A, &mdl, NULL, "Gate.1.Path.3");
-	mn_dst = mdl_add_path(TL_A, &mdl, NULL, "Gate");
-	mdl_add_path(TL_A, &mdl, NULL, "Gate.Start");
-	mdl_add_path(TL_A, &mdl, NULL, "Gate.Finish");
-
-	mn_src = mdl_add_path(TL_A, &mdl, NULL, "User");
-	mdl_add_path(TL_A, &mdl, NULL, "User.Manage");
-
-	mdl_add_path(TL_A, &mdl, NULL, "Gate.1");
-
 	mload_load(TL_A, &mdl, "etc/model.txt");
-
-	mdl_copy_node(TL_A, &mdl, mn_dst, "X", mn_src);
-	mdl_copy_path(TL_A, &mdl, mn_dst, "E", "Templates.Rule");
 
 	mdl_log_tree(TL_A, &mdl, NULL);
 
